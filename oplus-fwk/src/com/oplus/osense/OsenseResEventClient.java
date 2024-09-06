b7e6c2a85f584eef9e01149c3ca33ee49126614c
cjh1249131356 <cjh1249131356@gmail.com>
2024-09-06 20:59:32 +0800

    oplus-fwk: More bits for OnePlus Camera
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- oplus-fwk/src/com/oplus/osense/OsenseResEventClient.java
+++ oplus-fwk/src/com/oplus/osense/OsenseResEventClient.java
@@ -1,5 +1,8 @@
 package com.oplus.osense;
 
+import com.oplus.osense.eventinfo.EventConfig;
+import com.oplus.osense.eventinfo.OsenseEventCallback;
+
 public class OsenseResEventClient {
 
     private static OsenseResEventClient sInstance;
@@ -9,5 +12,13 @@ public class OsenseResEventClient {
             sInstance = new OsenseResEventClient();
         }
         return sInstance;
+    }
+
+    public int registerEventCallback(OsenseEventCallback callback, EventConfig eventConfig) {
+        return 0;
+    }
+
+    public int unregisterEventCallback(OsenseEventCallback callback, EventConfig eventConfig) {
+        return 0;
     }
 }
