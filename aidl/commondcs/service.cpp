LineageOS/android_hardware_oplus

Introduce vendor.oplus.hardware.commondcs-service
Change-Id: I4d91094cc2274a168b147a53d5ebe412a867e920

aidl/commondcs/service.cpp
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "CommonDcsAidlHalService.h"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using ::aidl::vendor::oplus::hardware::commondcs::CommonDcsAidlHalService;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<CommonDcsAidlHalService> reporter =
+            ndk::SharedRefBase::make<CommonDcsAidlHalService>();
+
+    const std::string instance = std::string() + CommonDcsAidlHalService::descriptor + "/default";
+    binder_status_t status =
+            AServiceManager_addService(reporter->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}

kaffuchino233/hardware_oplus

aidl/commondcs/service.cpp
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "CommonDcsAidlHalService.h"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using ::aidl::vendor::oplus::hardware::commondcs::CommonDcsAidlHalService;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<CommonDcsAidlHalService> service =
+            ndk::SharedRefBase::make<CommonDcsAidlHalService>();
+
+    const std::string instance = std::string() + CommonDcsAidlHalService::descriptor + "/oplus";
+    binder_status_t status =
+            AServiceManager_addService(service->asBinder().get(), instance.c_str());
+    CHECK_EQ(status, STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}